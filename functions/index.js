2c6bc7972e4553977c49ec4bd6c6789e778bb2cd
Josh Simmons <josh.simmons@gmail.com>
2018-05-22 00:13:53 -0700

    getBikes is working again. Moving towards getting addBike running.

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,15 @@ const Datastore = require('@google-cloud/datastore');
 // Instantiates a client
 const datastore = Datastore();
 
+const BIKE_KIND = 'Bike';
+
 // TODO: shared validation
 
 exports.getBikes = functions.https.onRequest((req, res) => {
-  const query = datastore.createQuery('Bike');
-  // TODO: share the bike query with addBike
+  const query = datastore
+    .createQuery(BIKE_KIND)
+    .filter('game', '=', 'Fremont');  // TODO: Read this from req.body.game
+
   return datastore.runQuery(query)
     .then((data) => {
       var entities = data[0];
@@ -20,10 +24,40 @@ exports.getBikes = functions.https.onRequest((req, res) => {
     });
 });
 
-/** Takes {user, location: {lat, lng, acc}, bikeId} */
+function getBike(game, bike) {
+  const query = datastore
+    .createQuery(BIKE_KIND)
+    .filter('game', '=', game)
+    .filter('bikeId', '=', bikeId);
+
+  return datastore.runQuery(query)
+    .then((data) => {
+      var entities = data[0];
+      if (entities.length > 1) {
+        throw new Error("Duplicate bike IDs found for game " +
+                        game + " bikeId " + bikeId);
+      }
+      if (entities.length === 0) {
+        return null;
+      }
+      return entities[0];
+    });
+}
+
+function saveBike(key, bike) {
+  const entity = {
+    key: key,
+    data: bike,
+  };
+
+  return datastore.save(entity);
+}
+
+/** Takes {game, user, location: {lat, lng, acc}, bikeId} */
 exports.addBike = functions.https.onRequest((req, res) => {
   var found = {};
   found.user = req.body.user;
+  found.game = req.body.game;
   found.location = {
     lat: parseFloat(req.body.location.lat),
     lng: parseFloat(req.body.location.lng),
@@ -36,37 +70,39 @@ exports.addBike = functions.https.onRequest((req, res) => {
   }
   */
   found.bikeId = req.body.bikeId;
-
-  const entity = {
-    key: datastore.key("Bike"),
-    data: found,
-  };
-
-  return datastore.save(entity)
-    .then((data) => {
-      return res.status(200).send("Claimed!")
-    })
-    .catch((err) => {
-      console.error(err);
-      return res.status(500).send(err.message);
-    });
   /*
   if (!validate.validateCode(found.bikeId)) {
     console.log('Invalid bikeId: ' + found.bikeId);
     return;
   }
-  for (var bike in bikes) {
-    if (bikes[bike].bikeId === found.bikeId) {
-      if (bikes[bike].user === found.user) {
-        res.send("You already own this bike!");
-        return;
-      } else {
-        res.send("Stolen!");
-        bikes[bike] = found
-        return;
-      }
-    }
-  }
-  bikes[found.bikeId] = found;
   */
+
+  getBike(found.game, found.bikeId)
+    .then(bike => {
+      // Simply add the bike if it's unclaimed.
+      if (bike === null) {
+        return saveBike(datastore.key(BIKE_KIND), found)
+          .then((data) => {
+            return res.status(200).send("Claimed!")
+          });
+      }
+
+      if (bike.user === found.user) {
+        return res.status(200).send("You already own this bike!");
+      } else {
+        const entity = {
+          key: bike[datastore.KEY],
+          data: found,
+        };
+
+        return saveBike(bike[datastore.KEY], bike)
+          .then((data) => {
+            return res.send("Stolen!");
+          });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send(err.message);
+    });
 });
